4bdbe3d77a63feaaa2f47ade6c85445620c52a57
Fernando German Torales Chorne <fgtoralesch@gmail.com>
2023-04-09 23:59:40 +0200

    Add retrieve page information function
    - Retrieved the information for the integrated page as a test
    - Documented in README important points to look up for more information to develop automations

---
+++ README.md
@@ -0,0 +1,13 @@
+## Notion Automations
+
+Sample code using Notion API to programatically read and write information to our databases
+
+# Setup
+
+To get started, it is necessary to add a config.yaml file to the .config folder. This must include the property "NOTION_KEY", including the value of the integration API key (See https://developers.notion.com/docs/authorization for more information).
+
+Then you can run notion_connect.py for running the examples
+
+# Where to go next
+
+Further information about the API use can be found in https://developers.notion.com/reference/intro. Check the "Endpoints" section for how to create, update and read pages and databases, as well as "Objects" for how to explore the fields within pages and databases.
--- src/notion_client.py
+++ src/notion_client.py
@@ -1,13 +1,17 @@
+"""
+Dev notes: 
+The Notion API does not yet support uploading files to Notion
+"""
+
 import os
 import requests
 import json
 import yaml
 
 
-class NotionConnectionManager:
+class NotionClient:
     def __init__(self):
         self.setup()
-        self.create_empty_subpage(title_text="Created from Python!")
 
     def setup(self):
         with open("../.config/config.yaml", "r") as config_params:
@@ -20,19 +24,21 @@ class NotionConnectionManager:
         self.headers = {'Authorization': f"Bearer {self.NOTION_KEY}", 
            'Content-Type': 'application/json', 
            'Notion-Version': '2022-06-28'}
+        
+        self.get_registered_pages()
 
-    def get_registered_integrations(self):
+    def get_registered_pages(self):
         search_params = {"filter": {"value": "page", "property": "object"}}
         search_response = requests.post(
             f'https://api.notion.com/v1/search', 
             json=search_params, headers=self.headers)
 
-        return search_response.json()["results"]
+        self.registered_pages = search_response.json()["results"]
+        self.first_page_id = self.registered_pages[0]["id"]
 
     def create_empty_subpage(self, title_text):
-        """ Notion API doesn't allow creating children of workspace, but only subpages """
-        search_results = self.get_registered_integrations()
-        page_id = search_results[0]["id"]
+        """ Retrieves first result of registered integrations and creates a new subpage """
+        page_id = self.first_page_id
 
         create_page_body = {
             "parent": { "page_id": page_id },
@@ -65,8 +71,15 @@ class NotionConnectionManager:
         create_response = requests.post(
             "https://api.notion.com/v1/pages", 
             json=create_page_body, headers=self.headers)
-        print(create_response.json())
-        # print(create_page_body)
+
+    def retrieve_page_info(self, page_id):
+        blocks_response = requests.get(
+            f"https://api.notion.com/v1/blocks/{self.first_page_id}/children", 
+            headers=self.headers)
+        print(blocks_response.json())
+
 
 if __name__ == "__main__":
-    notion_connection = NotionConnectionManager()
+    client = NotionClient()
+    # client.create_empty_subpage(title_text="Created from Python!")
+    # client.retrieve_page_info(page_id=integration.first_page_id)
